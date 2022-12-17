4e646a2d20db737e3f7654437d2d8411ec1d1ae4
Willian Guedes <willian.wg70@gmail.com>
2022-12-17 19:33:43 +0000

    feat: add image background as base to page

---
+++ landing/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <div className='image'>
+        <h3 className='text'>DIGITAL AGENCY</h3>
+      </div>
+    </div>
+  )
+}
+
+export default App
