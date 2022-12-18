279b30e055e6c731fc1839a770ea235f7bdd8652
Willian Guedes <willian.wg70@gmail.com>
2022-12-18 23:46:12 +0000

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ function App() {
 
   return (
     <div className='main'>
+      <header>
+        <h1 className="logo">Harvey.</h1>
+        <div className="menu">
+          <span> </span>
+          <span> </span>
+          <span> </span>
+        </div>
+      </header>
       <div className='image'>
         <h3 className='text'>DIGITAL AGENCY</h3>
       </div>
