55ea8e8a2b1d2f70c2f461f822775b4134743b67
Willian Guedes <willian.wg70@gmail.com>
2022-12-19 18:51:31 +0000

    feat: add description and animated button

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,10 @@ function App() {
       </header>
       <div className='image'>
         <h3 className='text'>DIGITAL AGENCY</h3>
+        <div className='description-div'>
+          <p className="description">Lorem ipsum dolor sit amet consectetur elit. <br/> Repellendus perferendis ducimus.</p>
+        </div>
+        <button className='btn-contact'>Contact Us</button>
       </div>
     </div>
   )
