b0dd5bc81d80c4c7e53e57827f9e3ce6b93fe384
Willian Guedes <willian.wg70@gmail.com>
2022-12-19 19:31:07 +0000

    feat: add base footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,36 @@ import './App.css'
 function App() {
 
   return (
-    <div className='main'>
-      <header>
-        <h1 className="logo">Harvey.</h1>
-        <div className="menu">
-          <span> </span>
-          <span> </span>
-          <span> </span>
+    <>
+      <div className='main'>
+        <header>
+          <h1 className="logo">Harvey.</h1>
+          <div className="menu">
+            <span> </span>
+            <span> </span>
+            <span> </span>
+          </div>
+        </header>
+        <div className='image'>
+          <h3 className='text'>DIGITAL AGENCY</h3>
+          <div className='description-div'>
+            <p className="description">Lorem ipsum dolor sit amet consectetur elit. <br /> Repellendus perferendis ducimus.</p>
+          </div>
+          <button className='btn-contact'>Contact Us</button>
         </div>
-      </header>
-      <div className='image'>
-        <h3 className='text'>DIGITAL AGENCY</h3>
-        <div className='description-div'>
-          <p className="description">Lorem ipsum dolor sit amet consectetur elit. <br/> Repellendus perferendis ducimus.</p>
-        </div>
-        <button className='btn-contact'>Contact Us</button>
       </div>
-    </div>
+      <footer>
+        <div className="footer-content">
+          <h3>Let's Talk?</h3>
+          <p className='description'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem et error accusamus rem molestiae. Corporis expedita iste deleniti earum non maxime blanditiis nulla dolorem exercitationem molestiae eveniet, voluptate ut cupiditate!</p>
+          <button className="btn-contact">Make An Enquiry</button>
+        </div>
+        <div className="rights">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </footer>
+    </>
   )
 }
 
