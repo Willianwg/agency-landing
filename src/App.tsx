c3a771eb9868605aabbbf6c80bc4d4cf8ecf927d
Willian Guedes <willian.wg70@gmail.com>
2022-12-26 23:37:46 +0000

    style: turn testimonials list into horizontal scrollable list

--- src/App.tsx
+++ src/App.tsx
@@ -161,6 +161,24 @@ function App() {
                 <IoStarSharp fill='orange' size={30} />
               </div>
             </div>
+
+            <div className="card">
+              <div className='client-information'>
+                <img src={ClientImage} />
+                <div className='client'>
+                  <p className="name">Bobby Fischer</p>
+                  <p className="occupation">Chess Master</p>
+                </div>
+              </div>
+              <p className="client-testimonial">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe enim esse fuga numquam possimus. Ipsa, repellendus. Labore nisi cumque, iure, reprehenderit dolor fuga quaerat dolorem expedita animi alias molestias tempora!</p>
+              <div className="stars">
+                <IoStarSharp fill='orange' size={30} />
+                <IoStarSharp fill='orange' size={30} />
+                <IoStarSharp fill='orange' size={30} />
+                <IoStarSharp fill='orange' size={30} />
+                <IoStarSharp fill='orange' size={30} />
+              </div>
+            </div>
           </div>
         </section>
       </div>
